16fd3d4aa2940377d1958f3a69a6b1e51b24429f
ClarenceAu <ozhencong@gmail.com>
2012-08-17 16:13:41 +0800

    hintchecker finish

--- hunter.js
+++ hunter.js
@@ -1,7 +1,37 @@
-var argument = require("./src/arguments"),
+var fs = require("fs"),
+    argument = require("./src/arguments"),
     filescaner = require("./src/filescaner"),
-    global;
+    hprocessor = require("./src/html-processor"),
+    cchecker = require("./src/customcheck"),
+    hchecker = require("./src/hintchecker"),
+    global, i, length;
 
 global = argument.parse(process.argv);
 global.files = filescaner.scanFile(global.dirPath, global.conf.fileExt, global.conf.ignoreFiles);
 // console.log(global);
+
+for(i = 0, length = global.files.length; i < length; i++) {
+    var file = global.files[i], 
+        fileText = fs.readFileSync(file, "utf-8"),
+        scripts, isHtml = false,
+        errorObj = {
+            fileName: file,
+            errors: []
+        };
+    if(global.debug) {
+        console.log("scanning " + file);
+    }
+    if(/\.html$/.test(file)) {
+        if(hprocessor.scratchJS(fileText)) {
+            scripts = hprocessor.scripts();
+        }
+        isHtml = true;
+    } else {
+        scripts = fileText;
+    }
+    // use jshint to scan javascirpt code
+    hchecker.check(scripts, global.conf.hint, global.conf.global);            
+    // use custom checker to scan all html and javascirpt code
+    cchecker.check(fileText, isHtml);   
+}
+console.log("scaning complete");
--- src/hintchecker.js
+++ src/hintchecker.js
@@ -4,5 +4,28 @@
  *
  */
 (function() {
-    var fs = require("fs"); 
+    var hint = require("../lib/jshint").JSHINT,
+        data = { errors: [] };
+
+    function check(scripts, options, global, minline) {
+        var lineNum = scripts.match(/\n/gi),
+            result = true;
+        if(minline && lineNum < minline) {
+            return true;
+        }
+        
+        result = hint(scripts, options, global);
+        if(!result) {
+            data.errors = hint.data().errors;
+        }
+
+        return result;
+    }
+
+    function errors() {
+        return data.errors;
+    }
+
+    exports.errors = errors;
+    exports.check = check;
 })();
