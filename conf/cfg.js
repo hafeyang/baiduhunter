30a442f1a1117d8456deaa929b51751d1c32a8db
hafeyang <yanghengfeng@163.com>
2012-08-21 14:01:05 +0800

    by-yhf update cfg.js

--- conf/cfg.js
+++ conf/cfg.js
@@ -29,7 +29,7 @@ module.exports={
         bitwise     : true, // if bitwise operators should not be allowed
         boss        : false, // if advanced usage of assignments should be allowed
         browser     : true, // if the standard browser globals should be predefined
-        camelcase   : true, // if identifiers should be required in camel case
+        camelcase   : false, // if identifiers should be required in camel case
         couch       : false, // if CouchDB globals should be predefined
         curly       : false, // if curly braces around all blocks should be required
         debug       : false, // if debugger statements should be allowed
@@ -47,11 +47,12 @@ module.exports={
         globalstrict: true, // if global "use strict"; should be allowed (also
                             // enables 'strict')
         immed       : false, // if immediate invocations must be wrapped in parens
+        indent      : false, // if indention must be checked
         iterator    : true, // if the `__iterator__` property should be allowed
         jquery      : true, // if jQuery globals should be predefined
         lastsemic   : true, // if semicolons may be ommitted for the trailing
                             // statements inside of a one-line blocks.
-        latedef     : true, // if the use before definition should not be tolerated
+        latedef     : false, // if the use before definition should not be tolerated
         laxbreak    : true, // if line breaks should not be checked
         laxcomma    : true, // if line breaks should not be checked around commas
         loopfunc    : true, // if functions should be allowed to be defined within
@@ -60,7 +61,7 @@ module.exports={
         mootools    : true, // if MooTools globals should be predefined
         multistr    : true, // allow multiline strings
         newcap      : true, // if constructor names must be capitalized
-        noarg       : true, // if arguments.caller and arguments.callee should be
+        noarg       : false, // if arguments.caller and arguments.callee should be
                             // disallowed
         node        : true, // if the Node.js environment globals should be
                             // predefined
@@ -68,12 +69,12 @@ module.exports={
         nonew       : true, // if using `new` for side-effects should be disallowed
         nonstandard : true, // if non-standard (but widely adopted) globals should
                             // be predefined
-        nomen       : true, // if names should be checked
+        nomen       : false, // if names should be checked
         onevar      : true, // if only one var statement per function should be
                             // allowed
         onecase     : true, // if one case switch statements should be allowed
         passfail    : false, // if the scan should stop on first error
-        plusplus    : true, // if increment/decrement should not be allowed
+        plusplus    : false, // if increment/decrement should not be allowed
         proto       : true, // if the `__proto__` property should be allowed
         prototypejs : true, // if Prototype and Scriptaculous globals should be
                             // predefined
@@ -81,8 +82,8 @@ module.exports={
                             // should be tolerated
         regexp      : true, // if the . should not be allowed in regexp literals
         rhino       : true, // if the Rhino environment globals should be predefined
-        undef       : true, // if variables should be declared before used
-        unused      : true, // if variables should be always used
+        undef       : false, // if variables should be declared before used
+        unused      : false, // if variables should be always used
         scripturl   : true, // if script-targeted URLs should be tolerated
         shadow      : true, // if variable shadowing should be tolerated
         smarttabs   : true, // if smarttabs should be tolerated
@@ -95,7 +96,7 @@ module.exports={
         validthis   : true, // if 'this' inside a non-constructor function is valid.
                             // This is a function scoped option only.
         withstmt    : true, // if with statements should be allowed
-        white       : true, // if strict whitespace rules apply
+        white       : false, // if strict whitespace rules apply
         worker      : true, // if Web Worker script symbols should be allowed
         wsh         : true  // if the Windows Scripting Host environment globals
                             // should be predefined
@@ -114,22 +115,19 @@ module.exports={
     	"_":true
     },
     errordef:{ //定义错误级别，在生成报表前可以过滤，调整某些错误的级别,级别有error,warning,ignore
-        "Unsafe character":"ignore",
-        "Mixed spaces and tabs":"warning",
-        "Too many var statements":"warning",
-        "Unexpected dangling":"ignore",
-        "to compare with":"ignore",
-        "is not defined":"ignore",
-        "Unexpected dangling":"ignore",
-        "A constructor name should start with an uppercase letter":"warning",
-        "Expected an assignment or function call and instead saw an expression":"warning",
+        "Unsafe character":"warining",
         "Mixed spaces and tabs":"ignore",
+        "Too many var statements":"warning",
+        "Insecure":"warning",
+        "to compare with":"ignore",
+        "Expected":" warning",
+        "Unescaped":" warning",
         "Missing semicolon":"warning",
         "console":"error",
-        "alert":"warning",
         "top.control":"warning",
         "timestamp missed":"warning",
-        "Missing space after": "warning"
+        'Unexpected space':'ignore',
+        'Missing space':'ignore'
     },
     minline: 10        // 定义最小的扫描行数，当文件的行数小于这个值时，不对文件进行检查，避免检查经过压缩的JS文件
 };
